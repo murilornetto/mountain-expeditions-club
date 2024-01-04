1fc25f2078d80c1382db7b0b482c755f39f88892
Murilo Russo Netto <murilornetto@gmail.com>
2024-01-04 18:22:50 -0300

    atualização geral

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function clickMenu(){
   if(itens.style.display == 'block'){
-      itens.style.display = 'none';  
+      itens.style.display = 'none';
       document.getElementById("burguer-icon").src = "img/menu-hamburguer.svg";
   }else{
       itens.style.display = 'block';
